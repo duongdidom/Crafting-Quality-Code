9f713ed79ac0fa23e0d4a100a88d253398e22b2d
duongdidom1 <cmduong.vn90+git@gmail.com>
2019-01-09 16:02:26 +1300

    there're 3 versions of scanrange, intermonth, intercomm: origin+old+new

--- ImpactTesting.py
+++ ImpactTesting.py
@@ -67,9 +67,9 @@ timestamp = pa2_pa2[len(parent_dir) + 1:][4:8]   # in format mmdd
 spanit_txt = parent_dir + r"\spanInstr.txt"
 
 # 4. read new parameter file
-# 4.1. define function to parse from each parameter sheet to 3 lists
-def parse_parameterWb_2_list(sheet):
-    # 4.1.1. insert every rows in the sheet to Para list
+### 4.1. define function to parse from each parameter sheet to 3 lists
+def parse_parameterWb_2_lists(sheet):
+    ### 4.1.1. insert every rows in the sheet to Para list
     rowscount = oldsheet.nrows
     colscount = oldsheet.ncols
     Para = []
@@ -80,9 +80,7 @@ def parse_parameterWb_2_list(sheet):
         Para.append(records)
         records = []
 
-    for o in Para: print (o)
-
-    # 4.1.2. append data from Para list into dictionary style lists
+    ### 4.1.2. append data from Para list into dictionary style lists
     scanrange = []
     intermonth = []
     intercomm = []
@@ -118,30 +116,81 @@ def parse_parameterWb_2_list(sheet):
                 'CommSpread':float(r[7])
             })
 
-    # 4.1.3. convert start date & end date in scan range list to yyyymm format
-    # datetime(*xlrd.xldate_as_tuple(r[3], 0)[0:5]).strftime("%Y%m") 
+    ### 4.1.3. convert start date & end date in scan range list 
+    for r in scanrange:
+        # convert general number to tuple
+        start_date = xlrd.xldate_as_tuple(r['StartDate'], 0)
+        end_date = xlrd.xldate_as_tuple(r['EndDate'], 0)
+
+        # convert to yyymm format
+        start_date = datetime(*start_date[0:5]).strftime("%Y%m") 
+        end_date = datetime(*end_date[0:5]).strftime("%Y%m") 
+
+        # update value for key StartDate and EndDate
+        r['StartDate'] = start_date
+        r['EndDate'] = end_date
 
     return scanrange, intermonth, intercomm
 
-# 4.2. read old sheet
+### 4.2. read old sheet & parse them to lists
 oldsheet = xlrd.open_workbook(TestPara_xlsx).sheet_by_name("Old")
-scanrange, intermonth, intercomm = parse_parameterWb_2_list(oldsheet)
+oldscanrange, oldintermonth, oldintercomm = parse_parameterWb_2_lists(oldsheet)
 
-# 4.2. read new sheet
+### 4.2. read new sheet
 newsheet = xlrd.open_workbook(TestPara_xlsx).sheet_by_name("New")
-
-
-
-# 5. parse them in 3 lists
+newscanrange, newintermonth, newintercomm = parse_parameterWb_2_lists(newsheet)
 
 # 6. read original pa2 file
+### 6.1. read and store each r into original_pa2
+with open (pa2_pa2, "r") as temp:
+    original_pa2 = temp.readlines()
 
-# 7. re write proposing pa2 file: old and new
+### 6.2. loop through each fron in original_pa2, store record into 3 lists
+origin_scanrange = []
+origin_intermonth = []
+origin_intercomm = []
 
-# # 8. make old and new subfolder
-# os.chdir(parent_dir)
-# os.mkdir(timestamp + " old")
-# os.mkdir(timestamp + " new")
+for r in original_pa2:
+    if r.startswith("B"):
+        origin_scanrange.append({
+            'comm':r[5:15].strip(),
+            'instype':r[15:18].strip(),
+            'maturity':int(r[18:24]),
+            'volscanrange':r[44:52],
+            'futscanrange':r[52:57]
+        })
+    elif r.startswith("C"):
+        origin_intermonth.append({
+            'comm':str(r[2:8]).strip(),
+            'priority':int(r[10:12].strip()),
+            'tiera':int(r[23:25].strip() or 0),
+            'deltaa':int(r[25:27].strip() or 0),
+            'tierb':int(r[30:32].strip() or 0),
+            'deltab':int(r[32:34].strip() or 0),
+            'spread':int(r[14:21].strip() or 0)
+        })
+    elif r.startswith("6"):
+        origin_intercomm.append({
+            'commgroup':str(r[2:5].strip()),
+            'priority':int(r[5:9].strip() or 0),
+            'comma':str(r[20:26]).strip(),
+            'deltaa':int(r[26:29].strip() or 0),
+            'commb':str(r[38:44]).strip(),
+            'deltab':int(r[44:47].strip() or 0),
+            'spread':int(r[9:12].strip() or 0)
+        })
+
+# 7. create old and new subfolder
+os.chdir(parent_dir)
+os.mkdir(timestamp + " old")
+os.mkdir(timestamp + " new")
+
+# 8. re write proposing pa2 file: old and new
+### 8.1. loop through original 3 lists, compare to old/new 3 lists
+### make amended list of record B, C, 6
+
+### 8.2. write old & new pa2 files in old & new directory
+
 
 # # 8. write SPAN instruction to re calculate risk array: old and new
 # spn = parent_dir + r"\test.spn"
